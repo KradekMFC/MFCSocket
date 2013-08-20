8999932a5f6c8b8cf827c1fcb8c8438ff7491f7d
Kradek <kradekmfc@gmail.com>
2013-08-20 14:51:46 -0400

    initial package

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = require('./lib/MFCSocket');
+module.exports.MFCMessage = require('./lib/MFCMessage');
+module.exports.MFCVideoState = require('./lib/MFCEnum').MFCVideoState;
+module.exports.MFCChatOpt = require('./lib/MFCEnum').MFCChatOpt;
+module.exports.MFCMessageType = require('./lib/MFCEnum').MFCMessageType;
+module.exports.MFCResponseType = require('./lib/MFCEnum').MFCResponseType;
