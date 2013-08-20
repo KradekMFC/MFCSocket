6c11e7aa77a808e30704c8a224110492a33f7a6b
Kradek <kradekmfc@gmail.com>
2013-08-20 15:53:40 -0400

    Add UserLookup shortcut class

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = require('./lib/MFCSocket');
+
 module.exports.MFCMessage = require('./lib/MFCMessage');
+module.exports.UserLookup = require('./lib/MFCMessage').UserLookup;
+
 module.exports.MFCVideoState = require('./lib/MFCEnum').MFCVideoState;
 module.exports.MFCChatOpt = require('./lib/MFCEnum').MFCChatOpt;
 module.exports.MFCMessageType = require('./lib/MFCEnum').MFCMessageType;
