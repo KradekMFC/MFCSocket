d00d07d0e40e920e2fdb393b05efc8a468f89f88
KradekMFC <kradekmfc@gmail.com>
2015-02-16 13:20:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ module.exports = require('./lib/MFCSocket');
 
 module.exports.MFCMessage = require('./lib/MFCMessage');
 module.exports.UserLookup = require('./lib/MFCMessage').UserLookup;
+module.exports.JoinChannelMessage = require('./lib/MFCMessage').JoinChannelMessage;
+module.exports.LeaveChannelMessage = require('./lib/MFCMessage').LeaveChannelMessage;
+
 
 module.exports.MFCVideoState = require('./lib/MFCEnum').MFCVideoState;
 module.exports.MFCChatOpt = require('./lib/MFCEnum').MFCChatOpt;
